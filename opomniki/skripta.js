a0336c17dd15e256421a5c65e8626be727b2cb2b
Simona <sm2279@student.uni-lj.si>
2016-03-08 11:41:27 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -20,7 +20,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#cas_opomnika").value = "";
 		
 		document.querySelector("#opomniki").innerHTML += " \
-			<div class='opomnik'> \
+			<div class='opomnik senca rob'> \
 				<div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
 				<div class='cas_opomnika'>Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> \
 			</div>"
@@ -40,6 +40,13 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if (cas == 0) {
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				alert("Opomnik!\n\nZadolžitev '" + naziv_opomnika + "'je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			} else {
+				casovnik.innerHTML = cas - 1;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
